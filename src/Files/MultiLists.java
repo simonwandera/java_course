7f37915c4c783f37fac52ca1a43486a9925bb297
simonwandera <simonwandera12@gmail.com>
2022-08-11 21:42:38 +0300

    Added loop through multidim array

--- src/Files/MultiLists.java
+++ src/Files/MultiLists.java
@@ -11,21 +11,24 @@ public class MultiLists {
         String data1 = "Dell, Hp, Acer, Lenovo";
         String data3 = "Mango, Beatroot, Grapes, Pineaples";
 
-
-
         ArrayList<String> cars = new ArrayList<String>(Arrays.asList(data0.split(",")));
         ArrayList<String> computers = new ArrayList<String>(Arrays.asList(data1.split(",")));
         ArrayList<String> fruits = new ArrayList<String>(Arrays.asList(data1.split(",")));
 
+        ArrayList<ArrayList<String>> bigData = new ArrayList();
 
+        bigData.add(cars);
+        bigData.add(computers);
+        bigData.add(fruits);
 
-
+        System.out.println("bigData = " + bigData);
         System.out.println("cars = " + cars);
         System.out.println("Array size = " + cars.size());
 
-
-        for(int i = 0; i < cars.size(); i++){
-            System.out.println(" item = " + cars.get(i));
+        for(int i=0; i < bigData.size(); i++){
+            for (int j=0; j<bigData.get(i).size(); j++){
+                System.out.println("Single items = " + bigData.get(i).get(j));
+            }
         }
 
 
