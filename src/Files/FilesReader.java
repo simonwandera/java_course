5a89cf4a75c9130d315c675190f7a012bac39d78
simonwandera <simonwandera12@gmail.com>
2022-08-13 17:00:48 +0300

    added menu items

--- src/Files/FilesReader.java
+++ src/Files/FilesReader.java
@@ -192,21 +192,28 @@ public class FilesReader {
         System.out.println("FG -> Filter by gender");
         System.out.println("FB -> Filter date of birth");
         System.out.println("MM -> Main menu");
+        System.out.println("XX -> Exit system");
 
         String Option = bufferedReader.readLine();
 
         if(Option.toLowerCase().equals("fi")){
             System.out.println("Filter By Id \n");
-            getDetails(PATH);
-        }else if (Option.toLowerCase().equals("a")){
-            System.out.println("All Patients \n");
-            getAllPatients(PATH);
-        }else if (Option.toLowerCase().equals("f")){
-            System.out.println("All Patients \n");
-            filterByDate(PATH);
-        }else if (Option.toLowerCase().equals("x")){
-            System.out.println("Exiting... \n");
-            System.exit(2);
+            filterById(PATH);
+        }else if (Option.toLowerCase().equals("fv")){
+            System.out.println("Filter by date of Visit \n");
+            filterByDateOfVisit(PATH);
+        }else if (Option.toLowerCase().equals("fg")){
+            System.out.println("Filter by gender \n");
+            filterByGender(PATH);
+        }else if (Option.toLowerCase().equals("fb")){
+            System.out.println("Filter by Date of Birth \n");
+            filterByDateOfBirth(PATH);
+        }else if (Option.toLowerCase().equals("mm")){
+            System.out.println("Main menu \n");
+            MainMenu(PATH);
+        }else if (Option.toLowerCase().equals("xx")){
+            System.out.println("Exit System ... \n");
+            System.exit(3);
 
         }else {
             System.out.println("The Option you selected was not recognized!! \n");
@@ -217,7 +224,7 @@ public class FilesReader {
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
         System.out.println("Enter Id Number to Find patient");
-        String dateString = bufferedReader.readLine();
+        String searchId = bufferedReader.readLine();
 
         ArrayList<ArrayList<String>> allPatients = readFile(PATH);
 
@@ -226,9 +233,8 @@ public class FilesReader {
             DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("YYYY-MM-dd");
             LocalDate dateOfBirth = LocalDate.parse((allPatients.get(i).get(3)), myFormatObj.BASIC_ISO_DATE);
             LocalDate dateOfVisit = LocalDate.parse((allPatients.get(i).get(5)), myFormatObj.BASIC_ISO_DATE);
-            LocalDate searchDate = LocalDate.parse(dateString, myFormatObj.BASIC_ISO_DATE);
 
-            if (dateOfVisit.equals(searchDate)){
+            if (allPatients.get(i).get(0).equals(searchId)){
                 System.out.println("ID Number : " + allPatients.get(i).get(0));
                 System.out.println("Name : " + allPatients.get(i).get(1));
                 System.out.println("Gender : " + allPatients.get(i).get(2));
@@ -251,8 +257,8 @@ public class FilesReader {
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        System.out.println("Enter Date in this format - YYYYMMDD");
-        String dateString = bufferedReader.readLine();
+        System.out.println("Enter Gender to search - (M/F)");
+        String searchGender = bufferedReader.readLine();
 
         ArrayList<ArrayList<String>> allPatients = readFile(PATH);
 
@@ -261,9 +267,8 @@ public class FilesReader {
             DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("YYYY-MM-dd");
             LocalDate dateOfBirth = LocalDate.parse((allPatients.get(i).get(3)), myFormatObj.BASIC_ISO_DATE);
             LocalDate dateOfVisit = LocalDate.parse((allPatients.get(i).get(5)), myFormatObj.BASIC_ISO_DATE);
-            LocalDate searchDate = LocalDate.parse(dateString, myFormatObj.BASIC_ISO_DATE);
 
-            if (dateOfVisit.equals(searchDate)){
+            if (allPatients.get(i).get(2).equals(searchGender)){
                 System.out.println("ID Number : " + allPatients.get(i).get(0));
                 System.out.println("Name : " + allPatients.get(i).get(1));
                 System.out.println("Gender : " + allPatients.get(i).get(2));
@@ -286,7 +291,7 @@ public class FilesReader {
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        System.out.println("Enter Date in this format - YYYYMMDD");
+        System.out.println("Enter Date of Birth in this format - YYYYMMDD");
         String dateString = bufferedReader.readLine();
 
         ArrayList<ArrayList<String>> allPatients = readFile(PATH);
