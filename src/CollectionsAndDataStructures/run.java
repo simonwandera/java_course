63b0cf747bd0468f2484c677c031c84cbe6fdde7
simonwandera <simonwandera12@gmail.com>
2022-08-23 22:58:18 +0300

    Implemented an iterator

--- src/CollectionsAndDataStructures/run.java
+++ src/CollectionsAndDataStructures/run.java
@@ -1,5 +1,7 @@
 package CollectionsAndDataStructures;
 
+import java.util.Iterator;
+
 public class run {
     public static void main(String[] args) {
         ListExample listExample = new ListExample();
@@ -7,8 +9,13 @@ public class run {
             listExample.addtoArraylist1("Added" + i);
         }
 
-        for (int j = 0; j < listExample.list1.size(); j++){
-            System.out.println(j);
+//        for (int j = 0; j < listExample.list1.size(); j++){
+//            System.out.println(j);
+//        }
+
+        Iterator itr = listExample.list1.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
         }
     }
 }
