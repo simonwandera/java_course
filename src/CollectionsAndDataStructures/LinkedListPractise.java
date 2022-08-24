09c5d4c92a116f7c568d717a4f48a48f18d8fc30
simonwandera <simonwandera12@gmail.com>
2022-08-24 16:16:28 +0300

    removing items from a linked list

--- src/CollectionsAndDataStructures/LinkedListPractise.java
+++ src/CollectionsAndDataStructures/LinkedListPractise.java
@@ -9,8 +9,8 @@ public class LinkedListPractise {
     public LinkedListPractise() {
 
         linkedList.addAll(arrayListPractise.getArrayList());
-
         linkedList.add(3, "Valeria");
+        linkedList.add("Kevin");
         linkedList.addLast("Kiche");
         linkedList.addFirst("Erastus");
         System.out.println(linkedList);
@@ -21,5 +21,28 @@ public class LinkedListPractise {
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
+    }
+
+    public void removeListItems(){
+        System.out.println("Removing kevin from the Linked List");
+        linkedList.remove("Kevin");
+        System.out.println(linkedList);
+        System.out.println("Removing item at index 2");
+        linkedList.remove(2);
+        System.out.println(linkedList);
+        System.out.println("Removing the first item");
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+        System.out.println("removing the last item");
+        linkedList.removeLast();
+        System.out.println(linkedList);
+        System.out.println("Removing the first occurence if item James");
+        linkedList.removeFirstOccurrence("James");
+        System.out.println(linkedList);
+        System.out.println("Removing the last occurence of item James");
+        linkedList.removeLastOccurrence("James");
+        System.out.println("clearing the linked list");
+        linkedList.clear();
+
     }
 }
