c578726eb51b446cf070f4cbc6df1834fffb6973
simonwandera <simonwandera12@gmail.com>
2022-08-24 13:48:13 +0300

    accessing an arraylist using the get metod

--- src/CollectionsAndDataStructures/ArrayListPractise.java
+++ src/CollectionsAndDataStructures/ArrayListPractise.java
@@ -6,12 +6,41 @@ import java.util.List;
 public class ArrayListPractise {
     List<String> arrayList = new ArrayList();
 
+    public ArrayListPractise() {
+
+        this.arrayList.add("Simon");
+        this.arrayList.add("Cloe");
+        this.arrayList.add("Mark");
+        this.arrayList.add("Kevin");
+        this.arrayList.add("Calvin");
+        this.arrayList.add("Japheth");
+        this.arrayList.add("Jackie");
+        this.arrayList.add("James");
+        this.arrayList.add("Paul");
+    }
+
     public void addToArraylist1(String st){
         arrayList.add(st);
     }
 
     public List<String> getArrayList() {
         return arrayList;
+    }
+
+    public void traverseUsingForEach(){
+        for(String str: arrayList){
+            System.out.println(str);
+        }
+    }
+    public void traverseUsingForeachMethod(){
+        arrayList.forEach(item ->{
+            System.out.println(item);
+        });
+    }
+
+    //accessing the element using get
+    public void accessingAnElement(int i){
+        System.out.println(arrayList.get(i));
     }
 
 }
--- src/CollectionsAndDataStructures/run.java
+++ src/CollectionsAndDataStructures/run.java
@@ -4,19 +4,9 @@ import java.util.Iterator;
 
 public class run {
     public static void main(String[] args) {
-        ArrayListPractise listExample = new ArrayListPractise();
-        for(int i = 1; i <= 50; i++){
-            listExample.addToArraylist1("item " + i);
-        }
-
-        Iterator itr = listExample.arrayList.iterator();
-        while (itr.hasNext()){
-            System.out.println(itr.next());
-        }
-
-        System.out.println("Traversing list through forEach() method:");
-        listExample.arrayList.forEach(a ->{
-            System.out.println(a);
-        });
+        ArrayListPractise arrayListPractise = new ArrayListPractise();
+        arrayListPractise.traverseUsingForEach();
+        arrayListPractise.traverseUsingForeachMethod();
+        arrayListPractise.accessingAnElement(2);
     }
 }
