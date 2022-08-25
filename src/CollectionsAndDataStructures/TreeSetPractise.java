8df530d28f4de83f09d6b3f6de51592d73b06ce6
simonwandera <simonwandera12@gmail.com>
2022-08-25 16:45:54 +0300

    Implemented iterating of items in a treeset using descending iterator

--- src/CollectionsAndDataStructures/TreeSetPractise.java
+++ src/CollectionsAndDataStructures/TreeSetPractise.java
@@ -17,7 +17,14 @@ public class TreeSetPractise {
     public void iteratingItemsInATreeSet(){
         Iterator<String> iterator = treeSet.iterator();
         while (iterator.hasNext()){
-            System.out.println(iterator.next());
+//            System.out.println(iterator.next());
+            break;
+        }
+//        using a descennding iterator
+
+        Iterator<String> descendingIterator = treeSet.descendingIterator();
+        while (descendingIterator.hasNext()){
+            System.out.println(descendingIterator.next());
         }
     }
 }
