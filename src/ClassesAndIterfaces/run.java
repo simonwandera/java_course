4f427b05fb649aed2df6d043ca88411a97124c35
simonwandera <simonwandera12@gmail.com>
2022-08-25 12:37:44 +0300

    added inputstream reader

--- src/ClassesAndIterfaces/run.java
+++ src/ClassesAndIterfaces/run.java
@@ -1,5 +1,9 @@
 package ClassesAndIterfaces;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class run {
 
     public static void main(String[] args) {
@@ -7,7 +11,24 @@ public class run {
 
         IAlien iAlien = new Alien("al23", "James");
 
-        iAlien.walk();
-        iAlien.run();
-    }
+//        iAlien.walk();
+//        iAlien.run();
+
+        Integer[] array = {2,4,6,7,8,5,3,4,7,5,2};
+
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+       
+
+
+
+
+        String names = "Bonnie, James, Mercy, George, Simon";
+
+        List<String> linkedlist = new LinkedList<>(Arrays.asList(names.split(",")));
+
+        System.out.println(linkedlist);
+
+     }
 }
