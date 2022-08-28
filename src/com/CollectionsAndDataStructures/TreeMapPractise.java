d18c4f339eb29ef024bd10f91c8bf6b79e171239
simonwandera <simonwandera12@gmail.com>
2022-08-28 09:12:18 +0300

    Added a tree map classFile

---
+++ src/com/CollectionsAndDataStructures/TreeMapPractise.java
@@ -0,0 +1,17 @@
+package com.CollectionsAndDataStructures;
+
+import java.util.TreeMap;
+
+public class TreeMapPractise {
+    private TreeMap<Integer, String> treeMap = new TreeMap<>();
+
+    public TreeMapPractise() {
+        treeMap.put(1, "Pig");
+        treeMap.put(2, "Pig");
+        treeMap.put(3, "Pig");
+        treeMap.put(4, "Pig");
+        treeMap.put(5, "Pig");
+        treeMap.put(6, "Pig");
+        treeMap.put(7, "Pig");
+    }
+}
