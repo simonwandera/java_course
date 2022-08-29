4bcc0a60a1b45fc9a0aee0294cb8953b6d5eaac6
simonwandera <simonwandera12@gmail.com>
2022-08-29 11:48:17 +0300

    traversing a list

---
+++ src/com/ExamSolution/Main.java
@@ -0,0 +1,10 @@
+package com.ExamSolution;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        solution.missingNumberInIntegerArray();
+        solution.traverseAList();
+    }
+}
--- src/com/ExamSolution/Solution.java
+++ src/com/ExamSolution/Solution.java
@@ -1,5 +1,41 @@
 package com.ExamSolution;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Solution {
+
+    public void missingNumberInIntegerArray(){
+        int [] integerArray = new int[100];
+
+        for(int i=0; i<= integerArray.length -1 ; i++ ){
+            int newNumber = new Random().nextInt(1, 101);
+            integerArray[i] = newNumber;
+//            System.out.println(i);
+        }
+
+        for(int i=0; i< integerArray.length; i++){
+            System.out.println(integerArray[i]);
+        }
+
+    }
+
+    public void traverseAList(){
+        List<String> cars = new ArrayList<>();
+        cars.add("Bughati");
+        cars.add("Dakuti");
+        cars.add("Tesla");
+        cars.add("Lamboghini");
+        cars.add("BMW");
+        cars.add("Lexus");
+
+//        Traversing a lisi using foreach method
+        cars.forEach(element ->{
+            System.out.println(element);
+        });
+
+    }
 
 }
