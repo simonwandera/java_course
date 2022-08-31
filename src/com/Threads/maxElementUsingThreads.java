25850e5205a9264062eb9dc8f6cef50cfa250f02
simonwandera <simonwandera12@gmail.com>
2022-08-31 15:01:15 +0300

    Implemented maximum fr the three threads

--- src/com/Threads/maxElementUsingThreads.java
+++ src/com/Threads/maxElementUsingThreads.java
@@ -2,31 +2,53 @@ package com.Threads;
 
 import java.util.Random;
 
+class WorkerThread extends Thread {
+
+    private int startIndex;
+    private int endIndex;
+    private int SIZE;
+    private int[] array = null;
+
+
+    public WorkerThread(int startIndex, int endIndex, int[] array) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+//        this.SIZE = SIZE;
+        this.array = array;
+    }
+
+    @Override
+    public void run() {
+
+        int max = array[startIndex];
+
+        for (int k = startIndex; k < endIndex +1 ; k++) {
+            if (array[k] > max)
+                max = array[k];
+        }
+
+        System.out.println("Maximum at thread " + Thread.currentThread().getId() + " is "+ max);
+    }
+}
+
+
 public class maxElementUsingThreads {
 
+    private static int maximum;
     public static void main(String[] args) {
+
         int SIZE = 16;
+
         int values[] = new int[SIZE];
-        for (int i=0; i<SIZE; i++){
+        for (int i = 0; i < SIZE; i++) {
             values[i] = new Random().nextInt(20);
         }
 
-//        Iterating the array
-
-        for (int j=0; j<values.length; j++){
-            System.out.println(values[j]);
-        }
-
-//        Find largest value
-        int max = values[0];
-
-        for (int k = 0; k< values.length; k++){
-            if (values[k] > max)
-                max =  values[k];
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println(values[i]);
         }
 
 //Chunks
-
         int chunkSize = SIZE/4;
 
         int startOfChunkOne = 0;
@@ -35,18 +57,20 @@ public class maxElementUsingThreads {
         int startOfChunkTwo = chunkSize ;
         int endOfChunkTwo = chunkSize + chunkSize -1 ;
 
-        int StartOfChunkThree = chunkSize + chunkSize;
+        int startOfChunkThree = chunkSize + chunkSize;
         int endOfChunkThree = chunkSize + chunkSize + chunkSize -1;
 
         int startOfChunkFour = chunkSize + chunkSize + chunkSize;
         int endOfChunkFour = chunkSize + chunkSize + chunkSize + chunkSize -1;
 
-
-        System.out.println("Chunk 1: " + startOfChunkOne + " " + endOfChunkOne);
-        System.out.println("Chunk 2: " + startOfChunkTwo + " " + endOfChunkTwo);
-        System.out.println("Chunk 3: " + StartOfChunkThree + " " + endOfChunkThree);
-        System.out.println("Chunk 4: " + startOfChunkFour + " " + endOfChunkFour);
+        WorkerThread t1 = new WorkerThread(startOfChunkOne, endOfChunkOne, values);
+        t1.start();
+        WorkerThread t2 = new WorkerThread(startOfChunkTwo, endOfChunkTwo, values);
+        t2.start();
+        WorkerThread t3 = new WorkerThread(startOfChunkThree, endOfChunkThree, values);
+        t3.start();
+        WorkerThread t4 = new WorkerThread(startOfChunkFour, endOfChunkFour, values);
+        t4.start();
 
     }
-
 }
