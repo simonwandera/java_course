a75f79b97a5712e844fffa6d8e33543dcc4a5b1e
simonwandera <simonwandera12@gmail.com>
2022-09-01 22:19:32 +0300

    added a linked list

--- src/com/Threads/ReverseHello.java
+++ src/com/Threads/ReverseHello.java
@@ -1,22 +1,38 @@
 package com.Threads;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 class ReverseWorkerThread extends Thread{
+
+    private static ArrayList<Integer> threadId;
     public ReverseWorkerThread() {
 //        super();
     }
+
+
     @Override
     public void run() {
         String message = "Hello from thread "+ Thread.currentThread().getId();
         System.out.println(message);
+
     }
 }
 public class ReverseHello {
+
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 50; i>0; i--){
+        LinkedList<ReverseWorkerThread> threadsList = new LinkedList<>();
+        for (int i = 0; i<50; i++){
             ReverseWorkerThread reverseWorkerThread = new ReverseWorkerThread();
-            reverseWorkerThread.start();
-            reverseWorkerThread.join();
+            threadsList.add(reverseWorkerThread);
+        }
 
-
+        Iterator threadsIterator = threadsList.descendingIterator();
+        while (threadsIterator.hasNext()){
+           ReverseWorkerThread r = (ReverseWorkerThread) threadsIterator.next();
+           r.start();
+           r.join();
         }
     }
 }
