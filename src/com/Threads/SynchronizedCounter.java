e8aa2f876f085c47ff6cfdfd3777706508293f53
simonwandera <simonwandera12@gmail.com>
2022-09-01 16:27:12 +0300

    Added a synchronized counter

---
+++ src/com/Threads/SynchronizedCounter.java
@@ -0,0 +1,17 @@
+package com.Threads;
+
+public class SynchronizedCounter {
+    private int c = 0;
+
+    public synchronized void increment() {
+        c++;
+    }
+
+    public synchronized void decrement() {
+        c--;
+    }
+
+    public synchronized int value() {
+        return c;
+    }
+}
