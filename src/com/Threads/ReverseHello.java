c0001dc87927bf12d97fe6883735d17b25114f90
simonwandera <simonwandera12@gmail.com>
2022-09-01 21:43:21 +0300

    decreasing i

--- src/com/Threads/ReverseHello.java
+++ src/com/Threads/ReverseHello.java
@@ -11,7 +11,7 @@ class ReverseWorkerThread extends Thread{
 }
 public class ReverseHello {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i<50; i++){
+        for (int i = 50; i>0; i--){
             ReverseWorkerThread reverseWorkerThread = new ReverseWorkerThread();
             reverseWorkerThread.start();
             reverseWorkerThread.join();
