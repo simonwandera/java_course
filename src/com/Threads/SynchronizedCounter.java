e9c19b1f45a4abc3a1aaafd72c59e0dc718a8061
simonwandera <simonwandera12@gmail.com>
2022-09-01 17:02:58 +0300

    Using synchronized statement to add synchronized code

--- src/com/Threads/SynchronizedCounter.java
+++ src/com/Threads/SynchronizedCounter.java
@@ -1,5 +1,10 @@
 package com.Threads;
 
+import jdk.internal.icu.text.UnicodeSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SynchronizedCounter {
     private int counter = 0;
 
@@ -13,5 +18,16 @@ public class SynchronizedCounter {
 
     public synchronized int value() {
         return counter;
+    }
+    
+//    Synchronized code using synchronized keyword
+    public void addName(String name) {
+        int nameCount = 0;
+        synchronized(this) {
+            String lastName = name;
+            nameCount++;
+        }
+        List<String> nameList = new ArrayList<>();
+        nameList.add(name);
     }
 }
