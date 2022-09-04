c84ce600bad5940f749272a158cea14a52d98e6c
simonwandera <simonwandera12@gmail.com>
2022-09-04 20:42:45 +0300

    Added printer class

--- src/com/Threads/PrintOdAndEvenNumber.java
+++ src/com/Threads/PrintOdAndEvenNumber.java
@@ -1,5 +1,13 @@
 package com.Threads;
 
+class Printer{
+    int counter = 1;
+    public void printer(){
+        System.out.println(counter);
+    }
+}
 public class PrintOdAndEvenNumber {
-    
+    public static void main(String[] args) {
+
+    }
 }
