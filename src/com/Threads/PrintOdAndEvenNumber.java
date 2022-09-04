cff7a789f068b94efeb44ac96a9130e538ac3460
simonwandera <simonwandera12@gmail.com>
2022-09-04 21:18:55 +0300

    Added a lock object

--- src/com/Threads/PrintOdAndEvenNumber.java
+++ src/com/Threads/PrintOdAndEvenNumber.java
@@ -2,6 +2,13 @@ package com.Threads;
 
 class Printer implements Runnable{
     static int counter = 1;
+    int reminder;
+    Object lock = new Object();
+
+    public Printer(int reminder) {
+        this.reminder = reminder;
+    }
+
     public void printer(){
         System.out.println(Thread.currentThread().getName() + " " + counter++);
     }
@@ -9,15 +16,23 @@ class Printer implements Runnable{
     @Override
     public void run() {
        for(int i = 1; i<=10;i++){
+           while (counter%2 != reminder ) {
+               try {
+                   lock.wait();
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+           }
             printer();
+           lock.notifyAll();
         }
     }
 }
 public class PrintOdAndEvenNumber {
     public static void main(String[] args) throws InterruptedException {
 
-        Thread even = new Thread(new Printer(), "Even thread");
-        Thread odd = new Thread(new Printer(), "Odd Thread");
+        Thread even = new Thread(new Printer(0), "Even thread");
+        Thread odd = new Thread(new Printer(1), "Odd Thread");
 
         even.start();
         odd.start();
