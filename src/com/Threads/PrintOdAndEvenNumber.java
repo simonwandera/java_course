fc83de29c73ce10bbe3088dbc7b957bb31e81837
simonwandera <simonwandera12@gmail.com>
2022-09-04 21:11:56 +0300

    Added a second arguments for thread for renaming

--- src/com/Threads/PrintOdAndEvenNumber.java
+++ src/com/Threads/PrintOdAndEvenNumber.java
@@ -1,25 +1,26 @@
 package com.Threads;
 
 class Printer implements Runnable{
-    int counter = 1;
+    static int counter = 1;
     public void printer(){
-        System.out.println(counter);
+        System.out.println(Thread.currentThread().getName() + " " + counter++);
     }
 
     @Override
     public void run() {
-        while (true){
+       for(int i = 1; i<=10;i++){
             printer();
         }
     }
 }
 public class PrintOdAndEvenNumber {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        Thread even = new Thread(new Printer());
-        Thread odd = new Thread(new Printer());
+        Thread even = new Thread(new Printer(), "Even thread");
+        Thread odd = new Thread(new Printer(), "Odd Thread");
 
         even.start();
         odd.start();
+
     }
 }
