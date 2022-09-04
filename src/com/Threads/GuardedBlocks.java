a1c985fd4a280fdb9651451084c5fd58152ad1d8
simonwandera <simonwandera12@gmail.com>
2022-09-04 20:40:51 +0300

    Added a file for printing odd and even numbers

--- src/com/Threads/GuardedBlocks.java
+++ src/com/Threads/GuardedBlocks.java
@@ -1,14 +1,51 @@
 package com.Threads;
 
-public class GuardedBlocks {
+class GuardedBlocksThread extends Thread {
 
-    Boolean joy;
+    @Override
+    public void run() {
+        guardedJoy();
+    }
 
-//    Wasteful and inefficient way
+    public static boolean joy;
+
+    //    Wasteful and inefficient way
     public void guardedJoy() {
         // Simple loop guard. Wastes
         // processor time. Don't do this!
-        while(!joy) {}
+        while (!joy) {
+        }
         System.out.println("Joy has been achieved!");
+    }
+
+//    A more efficient way!
+
+    public synchronized void notifyJoy() {
+        joy = true;
+        notifyAll();
+    }
+
+    public synchronized void guardedJoyEfficient() {
+        // This guard only loops once for each special event, which may not
+        // be the event we're waiting for.
+        while (!joy) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("Joy and efficiency have been achieved!");
+    }
+
+}
+
+public class GuardedBlocks{
+
+    public static void main(String[] args) {
+
+        new GuardedBlocksThread().guardedJoy();
+        new GuardedBlocksThread().notifyJoy();
+        new GuardedBlocksThread().guardedJoyEfficient();
+
     }
 }
---
+++ src/com/Threads/PrintOdAndEvenNumber.java
@@ -0,0 +1,5 @@
+package com.Threads;
+
+public class PrintOdAndEvenNumber {
+    
+}
