e3522ef291f59a8622c8cc2f03841d99ddf68412
simonwandera <simonwandera12@gmail.com>
2022-09-04 20:52:38 +0300

    Created thread objects

--- src/com/Threads/PrintOdAndEvenNumber.java
+++ src/com/Threads/PrintOdAndEvenNumber.java
@@ -1,13 +1,25 @@
 package com.Threads;
 
-class Printer{
+class Printer implements Runnable{
     int counter = 1;
     public void printer(){
         System.out.println(counter);
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            printer();
+        }
     }
 }
 public class PrintOdAndEvenNumber {
     public static void main(String[] args) {
 
+        Thread even = new Thread(new Printer());
+        Thread odd = new Thread(new Printer());
+
+        even.start();
+        odd.start();
     }
 }
