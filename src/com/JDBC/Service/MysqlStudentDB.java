49754493a6e703a846ec518e75ec8dec2e63f225
simonwandera <simonwandera12@gmail.com>
2022-09-12 12:25:51 +0300

    Created mysql JDBC db

--- src/com/JDBC/Service/MysqlStudentDB.java
+++ src/com/JDBC/Service/MysqlStudentDB.java
@@ -2,11 +2,27 @@ package com.JDBC.Service;
 
 import com.JDBC.Model.Student;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MysqlStudentDB implements IStudentDB{
+    private Connection connection;
+
+    public MysqlStudentDB() {
+        this.openConnection();
+    }
+
+    private boolean openConnection(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/patients", "root", "");
+            return true;
+        }catch (SQLException e){
+            return false;
+        }
+    }
     @Override
     public String createInsertQuery(Student student) {
         return null;
