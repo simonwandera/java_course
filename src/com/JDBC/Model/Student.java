5cdb42b3b7db5ca58a8202283ea193bf54e9a6a4
simonwandera <simonwandera12@gmail.com>
2022-09-12 11:59:46 +0300

    Created register student method

--- src/com/JDBC/Model/Student.java
+++ src/com/JDBC/Model/Student.java
@@ -1,5 +1,32 @@
 package com.JDBC.Model;
 
 public class Student {
-    
+    int id;
+    String name;
+    int IdNumber;
+    String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdNumber() {
+        return IdNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        IdNumber = idNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
---
+++ src/com/JDBC/Service/StudentUI.java
@@ -0,0 +1,22 @@
+package com.JDBC.Service;
+
+import com.JDBC.Model.Student;
+
+import java.util.Scanner;
+
+public class StudentUI {
+
+    static Scanner scanner;
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void registerStudent(){
+        Student student = new Student();
+        System.out.println("Enter student ID Number; ");
+        
+        System.out.println("Enter student Name; ");
+        System.out.println("Enter student Gender; ");
+    }
+}
