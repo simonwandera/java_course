a65377285ba28d01022319a80aeb87c4297e37da
simonwandera <simonwandera12@gmail.com>
2022-09-14 20:42:22 +0300

    Added a cat class for generics function

--- src/com/Generics/YoutubeTrial/Cat.java
+++ src/com/Generics/YoutubeTrial/Cat.java
@@ -1,4 +1,19 @@
 package com.Generics.YoutubeTrial;
 
 public class Cat {
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/com/Generics/YoutubeTrial/Main.java
+++ src/com/Generics/YoutubeTrial/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Printer printString = new Printer("Something to print");
         Printer printInteger = new Printer(23);
+        Printer catPrinter = new Printer(new Cat("Mews", 23));
         printString.print();
         printInteger.print();
+        catPrinter.print();
     }
 }
