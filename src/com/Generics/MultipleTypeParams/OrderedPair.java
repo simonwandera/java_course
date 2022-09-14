1c1a09eef3a892fe68f4b20f58766d492bb6e0a6
simonwandera <simonwandera12@gmail.com>
2022-09-14 14:41:40 +0300

    Added a multiple type class

---
+++ src/com/Generics/MultipleTypeParams/OrderedPair.java
@@ -0,0 +1,19 @@
+package com.Generics.MultipleTypeParams;
+
+interface Pair<K, V> {
+    public K getKey();
+    public V getValue();
+}
+public class OrderedPair<K, V> implements Pair<K, V> {
+
+    private K key;
+    private V value;
+
+    public OrderedPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey()	{ return key; }
+    public V getValue() { return value; }
+}
--- src/com/Generics/MultipleTypeParams/Pair.java
+++
@@ -1,4 +0,0 @@
-package com.Generics.MultipleTypeParams;
-
-public interface Pair {
-}
