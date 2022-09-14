2d7f20af060fab58cfac0895fff4b037a0ec1535
simonwandera <simonwandera12@gmail.com>
2022-09-14 13:59:06 +0300

    Generics to demonstrate castong

--- src/com/Generics/GenericsPractise.java
+++ src/com/Generics/GenericsPractise.java
@@ -1,4 +1,20 @@
 package com.Generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsPractise {
+    public static void main(String[] args) {
+//        The following code without generics requires casting
+        List list = new ArrayList();
+        list.add("hello");
+        String s = (String) list.get(0);
+//        System.out.println(s);
+
+//        When re-written to use generics, the code does not require casting:
+        List<String> list1 = new ArrayList<String>();
+        list1.add("hello");
+        String string = list1.get(0);   // no cast
+        System.out.println(string);
+    }
 }
