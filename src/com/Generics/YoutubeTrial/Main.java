6ed5e84884c62a71b0d5e5ca7289f5157f40451e
simonwandera <simonwandera12@gmail.com>
2022-09-14 21:19:23 +0300

    Added parameters to the generic class

--- src/com/Generics/YoutubeTrial/Main.java
+++ src/com/Generics/YoutubeTrial/Main.java
@@ -1,12 +1,22 @@
 package com.Generics.YoutubeTrial;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Printer printString = new Printer("Something to print");
-        Printer printInteger = new Printer(23);
-        Printer catPrinter = new Printer(new Cat("Mews", 23));
+        Printer<String> printString = new Printer<>("Something to print");
+        Printer<Integer> printInteger = new Printer<>(23);
+        Printer<Cat> catPrinter = new Printer<>(new Cat("Mews", 23));
+
+        List<String> names = new ArrayList<>();
+        names.add("Caleb");
+        names.add("Victor");
+        Printer<List<String>> listPrinter = new Printer<>(names);
+
         printString.print();
         printInteger.print();
         catPrinter.print();
+        listPrinter.print();
     }
 }
