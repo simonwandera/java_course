e50c2a56a79f0a40cef4a12ced9445405e2b6152
simonwandera <simonwandera12@gmail.com>
2022-09-14 16:02:11 +0300

    Generic Methods and Bounded Type Parameters

--- src/com/Generics/BoundedBox.java
+++ src/com/Generics/BoundedBox.java
@@ -16,9 +16,17 @@ public class BoundedBox <T>{
         System.out.println("U: " + u.getClass().getName());
     }
 
+    public static <T> int countGreaterThan(T[] anArray, T elem) {
+        int count = 0;
+        for (T e : anArray)
+            if (e > elem)  // compiler error
+                ++count;
+        return count;
+    }
+
     public static void main(String[] args) {
         BoundedBox<Integer> integerBox = new BoundedBox<Integer>();
         integerBox.set(new Integer(10));
-        integerBox.inspect("some text"); // error: this is still String!
+//        integerBox.inspect("Some text"); // error: this is still String!
     }
 }
