de7f948973f12e4c5a9339f3a4ff83b2cc758748
simonwandera <simonwandera12@gmail.com>
2022-09-21 13:36:29 +0300

    Generated an enumMap class

--- src/com/CollectionsAndDataStructures/EnumMapPractise.java
+++ src/com/CollectionsAndDataStructures/EnumMapPractise.java
@@ -1,5 +1,18 @@
 package com.CollectionsAndDataStructures;
 
+import java.util.EnumMap;
+
+enum Days{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday ,Sunday
+}
+
 public class EnumMapPractise {
-    
+
+    public void generateEnumMap(){
+        EnumMap<Days, String> map = new EnumMap<Days, String>(Days.class);
+        map.put(Days.Monday, "1");
+        map.put(Days.Thursday, "2");
+        map.put(Days.Wednesday, "3");
+        map.put(Days.Thursday, "4");
+    }
 }
