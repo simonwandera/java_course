0503a2594b0f4abd621722439dfd354a5ba8e811
simonwandera <simonwandera12@gmail.com>
2022-09-21 16:09:03 +0300

    Testing user defined class with compatable method

--- src/com/CollectionsAndDataStructures/ComparableExample.java
+++ src/com/CollectionsAndDataStructures/ComparableExample.java
@@ -1,13 +1,31 @@
 package com.CollectionsAndDataStructures;
 
 class Meal implements Comparable<Meal>{
+    private String mealName;
+    private int orders;
+    private String nutrients;
+
+    public Meal(String mealName, String nutrients, int orders) {
+        this.mealName = mealName;
+        this.nutrients = nutrients;
+        this.orders = orders;
+    }
 
     @Override
-    public int compareTo(Meal o) {
-        return 0;
+    public int compareTo(Meal meal) {
+        if(orders == meal.orders)
+            return 0;
+        else if (orders > meal.orders)
+            return 1;
+        else
+            return -1;
     }
 }
 
 public class ComparableExample {
+
+    private void sortMeals(){
+
+    }
 
 }
